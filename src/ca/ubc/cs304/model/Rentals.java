11926c3cc08b64dd4eddb095bfc20de8b1f28c7d
danhuijia <l5y0b@ugrad.cs.ubc.ca>
2019-11-20 18:58:37 -0800

    added 5 classes

--- src/ca/ubc/cs304/model/Rentals.java
+++ src/ca/ubc/cs304/model/Rentals.java
@@ -1,4 +1,93 @@
 package ca.ubc.cs304.model;
 
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ * The intent for this class is to update/store information about a single Customer
+ */
+
+/**
+ * A customer is identified by its dlicense
+ *
+ */
 public class Rentals {
+    private final int rid;
+    private final String  vlicense; // vehicle is now identified by vlicense -> old: vid
+    private final String dlicense; // customer is now identified by dlicense -> old: cellphone
+    private final Date fromDate;
+    private final Time fromTime;
+    private final Date toDate;
+    private final Time toTime;
+    private final int odometer;
+    private final String cardName;
+    private final int cardNo;
+    private final Date expDate;
+    private final int confNo;
+
+
+    public Rentals(int rid, String vlicense, String dlicense, Date fromDate, Time fromTime,
+                   Date toDate, Time toTime, int odometer, String cardName, int cardNo, Date expDate, int confNo) {
+        this.rid = rid;
+        this.vlicense = vlicense;
+        this.dlicense = dlicense;
+        this.fromDate = fromDate;
+        this.fromTime = fromTime;
+        this.toDate = toDate;
+        this.toTime = toTime;
+        this.odometer = odometer;
+        this.cardName = cardName;
+        this.cardNo = cardNo;
+        this.expDate = expDate;
+        this.confNo = confNo;
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public String getVlicense() {
+        return vlicense;
+    }
+
+    public String getDlicense() {
+        return dlicense;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public Time getToTime() {
+        return toTime;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public Time getFromTime() {
+        return fromTime;
+    }
+
+    public Integer getOdometer() {
+        return odometer;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public Integer getCardNo() {
+        return cardNo;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
+
+    public Integer getConfNo() {
+        return confNo;
+    }
+
 }
--- src/ca/ubc/cs304/model/VehicleTypes.java
+++ src/ca/ubc/cs304/model/VehicleTypes.java
@@ -1,4 +1,73 @@
 package ca.ubc.cs304.model;
 
+/**
+ * The intent for this class is to update/store information about a single VehicleType
+ */
+
+/**
+ * A vehicle is identified by its vlicense
+ *
+ */
 public class VehicleTypes {
+    private final String vlicense;  // vtname -> vlicense instead??
+    private final String features;
+    private final int wrate;
+    private final int drate;
+    private final int hrate;
+    private final int wirate;
+    private final int dirate;
+    private final int hirate;
+    private final int kirate;
+
+
+    public VehicleTypes(String vlicense, String features, int wrate, int drate, int hrate, int wirate,
+                        int dirate, int hirate, int kirate) {
+        this.vlicense = vlicense;
+        this.features = features;
+        this.wrate = wrate;
+        this.drate = drate;
+        this.hrate = hrate;
+        this.wirate = wirate;
+        this.dirate = dirate;
+        this.hirate = hirate;
+        this.kirate = kirate;
+    }
+
+    public String getVlicense() {
+        return vlicense;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public Integer getWrate() {
+        return wrate;
+    }
+
+    public Integer getDrate() {
+        return drate;
+    }
+
+    public Integer getHrate() {
+        return hrate;
+    }
+
+    public Integer getWirate() {
+        return wirate;
+    }
+
+    public Integer getDirate() {
+        return dirate;
+    }
+
+
+    public Integer getHirate() {
+        return hirate;
+    }
+
+    public Integer getKirate() {
+        return kirate;
+    }
+
 }
